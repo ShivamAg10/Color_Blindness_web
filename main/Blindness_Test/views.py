448fa0ae73567f07b7296800c7a9957ef0085cfe
Shivam1848 <kashokagrawal1848@gmail.com>
2025-07-19 14:15:43 +0530

    added more login credentials in urls

--- main/Blindness_Test/views.py
+++ main/Blindness_Test/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url="/Accounts/Login")
 def test(request):
     return render(request, "Blindness_Test/test.html")
 
+@login_required(login_url="/Accounts/Login")
 def quiz(request):
     return render(request, "Blindness_Test/quiz.html")
--- main/objec/views.py
+++ main/objec/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url="/Accounts/Login")
 def object(request):
     return render(request, "objec/object.html")
